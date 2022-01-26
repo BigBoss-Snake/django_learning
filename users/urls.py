2e1573e1a8bfcfa30d82b0c644f10f1bc0f6882a
Andrey <gorbachyow.andrei@yandex.ru>
2022-01-26 13:56:11 +0300

    Add enpoint for login user

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import RegistrationAPIView
+from .views import RegistrationAPIView, LoginAPIView
 
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('create/', RegistrationAPIView.as_view(), name = 'index'), 
+    path('create/', RegistrationAPIView.as_view(), name = 'create'), 
+    path('login/', LoginAPIView.as_view(), name = 'login'), 
 ]
