e8a8a6f131cbfa2e5f5cd6c3186db1d49a97e148
Andrey <gorbachyow.andrei@yandex.ru>
2022-02-04 15:03:29 +0300

    Add list and update endpoint

--- book/urls.py
+++ book/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
-from .views import CreateBook, DeleteBook, DetaelBook
+from .views import CreateBook, DeleteBook, DetaelBook, ListBook, UpdateBook
 
 
 urlpatterns = [
-    # path('', views.BookListView.as_view(), name='books'),
+
     path('create/', CreateBook.as_view(), name = 'create'),
-    # path('list/', views.index, name='index'),
+    path('list/', ListBook.as_view(), name='list'),
     path('<int:book_id>/', DetaelBook.as_view(), name='detail'),
-    # path('update/', views.update, name='index'),
-    # path('update/create/', views.create),
+    path('update/', UpdateBook.as_view(), name='update'),
     path('delete/<int:book_id>/', DeleteBook.as_view(), name='delete'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,8 @@
+
 from django.shortcuts import render
 from rest_framework.response import Response
+
+from users.models import User
 from .models import Books
 from .renderers import BooksJSONRenderer
 from django.http import JsonResponse, HttpResponseRedirect
@@ -8,6 +11,9 @@ from rest_framework.views import APIView
 from .serialazers import BookSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework import status
+
+from book import serialazers
+
 
 
 class CreateBook(APIView):
@@ -32,7 +38,7 @@ class DetaelBook(APIView):
     def get(self, request, book_id):
         book = Books.objects.get(id = book_id)
         serializer = BookSerializer(book)
-        return Response({'book' : serializer.data}, status = status.HTTP_200_OK)
+        return JsonResponse({'book' : serializer.data}, status = status.HTTP_200_OK)
 
 
 class DeleteBook(APIView):
@@ -41,3 +47,36 @@ class DeleteBook(APIView):
         book = Books.objects.get(id = book_id)
         book.delete()
         return Response(f'Your delete book {book.title}', status = status.HTTP_202_ACCEPTED)
+
+
+class ListBook(APIView):
+
+    permission_classes = (AllowAny,)
+    serializer_class = BookSerializer
+
+    def get(self, request):
+        all_book = Books.objects.all()
+
+        dict_book = []
+
+        for i in range(len(all_book)):
+            dict_book.append({'book' : BookSerializer(all_book[i]).data})
+
+        return Response(dict_book, status=status.HTTP_200_OK)
+
+
+class UpdateBook(APIView):
+
+    permission_classes = (AllowAny,)
+    serializer_class = BookSerializer
+
+    def put(self, request):
+        book = request.data.get('book', {})
+        serializer = self.serializer_class(data=book)
+        serializer.is_valid(raise_exception=True)
+        update_book = Books.objects.get(title = serializer.data.get('title'))
+        update_book.author_book = serializer.data.get('author_book')
+        update_book.save()
+        serializers = BookSerializer(update_book)
+
+        return Response({'book' : serializers.data}, status=status.HTTP_200_OK)
